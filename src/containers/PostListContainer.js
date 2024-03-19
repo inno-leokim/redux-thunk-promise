02814e80f84fdb3291efff335656ad7f6c8ad9dd
inno-leokim <inno.leokim@gmail.com>
2024-03-19 17:53:52 +0900

    [EDIT] PostListContainer에서 가져오는 리듀서 데이터 항목을 post에서 posts로 수정

--- src/containers/PostListContainer.js
+++ src/containers/PostListContainer.js
@@ -4,7 +4,7 @@ import PostList from "../components/PostList";
 import { getPosts } from "../modules/posts";
 
 function PostListContainer() {
-    const { data, loading, error } = useSelector(state => state.posts.post);
+    const { data, loading, error } = useSelector(state => state.posts.posts);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -14,6 +14,8 @@ function PostListContainer() {
     if (loading) return <div>로딩중...</div>;
     if (error) return <div>에러 발생!</div>;
     if (!data) return null;
+
+
     return <PostList posts={data} />;
 }
 
