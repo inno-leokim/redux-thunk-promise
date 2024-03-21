ed85f19da38dbfb49dac7440e9cd4fa115eac136
inno-leokim <inno.leokim@gmail.com>
2024-03-21 09:52:27 +0900

    [ADD] Page Roting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import PostListContainer from "./containers/PostListContainer";
+import { Route, Routes } from "react-router-dom";
+import PostPage from "./pages/PostPage";
+import PostListPage from "./pages/PostListPage";
 
 function App() {
   
   return (
     <div className="App">
-      <PostListContainer />
+      <Routes>
+        <Route path="/" element={<PostListPage />}/>
+        <Route path="/:id" element={<PostPage />}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function PostList({ posts }){
     return (
@@ -8,7 +9,7 @@ function PostList({ posts }){
                     post => 
                         (
                             <li key={post.id}>
-                                {post.title}
+                                <Link to={`/${post.id}`}>{post.title}</Link>
                             </li>
                         )
                 )
